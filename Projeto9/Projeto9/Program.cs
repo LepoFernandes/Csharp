44f1c3ac96327f461cdaf556e632d769add4580f
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2024-04-05 02:33:01 -0300

    novo ex

---
+++ Projeto9/Projeto9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com a largura do terreno:");
+            double L = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com o comprimento do terreno:");
+            double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("O terreno possui " + L + "m de largura e " + C + "m de comprimento.");
+
+            Console.WriteLine("Agora digite o valor do metro quadrado:");
+            double P = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Metro quadrado vale: " + P.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
+
+            double Area = L * C;
+            double Ptotal = Area * P;
+
+            Console.WriteLine("A area total do terreno : " + Area.ToString("F2", CultureInfo.InvariantCulture) + " metros quadrados." );
+            Console.WriteLine("O valor total do terreno : " + Ptotal.ToString("F2", CultureInfo.InvariantCulture) + " reais. ");
+            
+        }
+    }
+}
