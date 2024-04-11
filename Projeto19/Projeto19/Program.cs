89218ecca16f991e8ae2c31ef5e097929e6fa341
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2024-04-11 12:14:51 -0300

    new

---
+++ Projeto19/Projeto19/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = x / y;
+
+            Console.WriteLine(media.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
