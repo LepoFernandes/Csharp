867d3627b6dc956681ba35605717f144de66447b
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2024-04-12 14:52:19 -0300

    bora

---
+++ Projeto20/Projeto20/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia = int.Parse(Console.ReadLine());
+
+            int tempo = distancia * 2;
+
+            Console.WriteLine(tempo + " minutos");
+        }
+    }
+}
---
+++ Projeto21/Projeto21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int media, tempo, velocidade;
+
+            media = 12;
+            tempo = int.Parse(Console.ReadLine());
+            velocidade = int.Parse(Console.ReadLine());
+
+            double distancia = tempo * velocidade;
+
+            double litros = distancia / media;
+
+            Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
