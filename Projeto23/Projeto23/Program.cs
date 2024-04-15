e297f24c6600471ab72ad8adfafe77311881c582
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2024-04-15 13:01:04 -0300

    novos exercicios

---
+++ Projeto23/Projeto23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] dados1 = Console.ReadLine().Split(' ');
+            string[] dados2 = Console.ReadLine().Split(' ');
+
+            string nome1 = dados1[0];
+            int idade1 = int.Parse(dados1[1]);
+
+            string nome2 = dados2[0];
+            int idade2 = int.Parse(dados2[1]);
+
+            double media = (double)(idade1 + idade2) / 2;
+
+            Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + media.ToString("F1", CultureInfo.InvariantCulture) + " anos. ");
+        }
+    }
+}
