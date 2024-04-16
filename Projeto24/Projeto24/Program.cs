b38112581c4455e775452e8362889ea39719ccca
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2024-04-16 16:33:01 -0300

    exercicio

---
+++ Projeto24/Projeto24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double bonus = (15 * vendas) / 100;
+
+            double total = bonus + salario;
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Projeto26/Projeto26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            double A = double.Parse(valores[0], CultureInfo.InvariantCulture) ;
+            double B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            double triangulo = 0.5 * A * C;
+            double circulo = 3.14159 * C * C;
+            double trapezio = 0.5 * (A + B) * C;
+            double quadrado = B * B;
+            double retangulo = A * B;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
