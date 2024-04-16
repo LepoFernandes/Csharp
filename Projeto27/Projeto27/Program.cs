e923219215b8a3618d8c6919b21e02279af299d4
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2024-04-16 16:53:24 -0300

    novos

---
+++ Projeto27/Projeto27/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+            int C = int.Parse(valores[2]);
+
+            int MaiorAB = (A + B + Math.Abs(A - B)) / 2;
+            int Maior = (MaiorAB + C + Math.Abs(MaiorAB - C)) / 2;
+
+            Console.WriteLine(Maior + " eh o maior");
+        }
+    }
+}
---
+++ Projeto28/Projeto28/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] p1 = Console.ReadLine().Split(' ');
+            string[] p2 = Console.ReadLine().Split(' ');
+
+            double x1 = double.Parse(p1[0], CultureInfo.InvariantCulture);
+            double y1 = double.Parse(p1[1], CultureInfo.InvariantCulture);
+
+            double x2 = double.Parse(p2[0], CultureInfo.InvariantCulture);
+            double y2 = double.Parse(p2[1], CultureInfo.InvariantCulture);
+
+            double distancia = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            Console.WriteLine(distancia.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
