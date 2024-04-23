890d6340b42d8bcfb036ff5edc5cda5867a3ad84
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2024-04-23 00:42:55 -0300

    new

---
+++ Projeto34/Projeto34/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva as duas notas para saber o resultado:")
+
+            double notaUm = double.parse(Console.ReadLine());
+            double notaDois = double.parse(Console.ReadLine());
+
+            double notaTotal = notaUm + notaDois;
+
+            if (notaTotal > 60.0)
+            {
+                Console.WriteLine("NOTA FINAL = " + notaTotal.toString("F1", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                Console.WriteLine("NOTA FINAL = " + notaTotal.ToString("F1", CultureInfo.InvariantCulture);
+                Console.WriteLine("REPROVADO")
+            }
+        }
+    }
+}
