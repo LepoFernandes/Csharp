6252b8b9b002d1b0e0af83312f09db1e7e0329f1
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-04-24 16:21:32 -0300

    update

---
+++ Projeto35/Projeto35/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] coeficientes = Console.ReadLine().Split(' ');
+
+            double a = double.Parse(coeficientes[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(coeficientes[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(coeficientes[2], CultureInfo.InvariantCulture);
+
+            double delta = (b * b) - 4 * a * c;
+
+            if ( delta < 0 || a == 0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            } else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                Console.WriteLine("R1 = " + x1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + x2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ Projeto36/Projeto36/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numeros = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(numeros[0]);
+            int b = int.Parse(numeros[1]); 
+            int c = int.Parse(numeros[2]);
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine("Menor = " + a);
+            }else if (b < a && b < c)
+            {
+                Console.WriteLine("Menor = " + b);
+            }else
+            {
+                Console.WriteLine("Menor = " + c);
+            }
+        }
+    }
+}
