ac328542a8ed593d26385d6594426cedac827c25
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-04-29 12:54:28 -0300

    update

---
+++ Projeto45/Projeto45/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] horas = Console.ReadLine().Split(' ');
+
+            int horaInicial = int.Parse(horas[0]);
+            int horaFinal = int.Parse(horas[1]);
+
+            int duracao;
+
+            if (horaFinal > horaInicial)
+            {
+                duracao = horaFinal - horaInicial;
+
+            }else if (horaFinal == horaInicial )
+            {
+                duracao = 24;
+
+            }else
+            {
+                duracao = (24 - horaInicial) + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " +  duracao + " HORA(S)");
+        }
+    }
+}
