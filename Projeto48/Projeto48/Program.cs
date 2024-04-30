90c1affa3575bde6a4846f7a8089e4c624900182
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-04-30 17:26:05 -0300

    exercicios

---
+++ Projeto48/Projeto48/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string entrada1 = Console.ReadLine();
+            string entrada2 = Console.ReadLine();
+            string entrada3 = Console.ReadLine();
+            string animal = "";
+
+            if (entrada1 == "vertebrado")
+            {
+                if (entrada2 == "ave")
+                {
+                    if (entrada3 == "carnivoro")
+                        animal = "aguia";
+                    else if (entrada3 == "onivoro")
+                        animal = "pomba";
+                }
+                else if (entrada2 == "mamifero")
+                {
+                    if (entrada3 == "onivoro")
+                        animal = "homem";
+                    else if (entrada3 == "herbivoro")
+                        animal = "vaca";
+                }
+            }
+            else if (entrada1 == "invertebrado")
+            {
+                if (entrada2 == "inseto")
+                {
+                    if (entrada3 == "hematofago")
+                        animal = "pulga";
+                    else if (entrada3 == "herbivoro")
+                        animal = "lagarta";
+                }
+                else if (entrada2 == "anelideo")
+                {
+                    if (entrada3 == "hematofago")
+                        animal = "sanguessuga";
+                    else if (entrada3 == "onivoro")
+                        animal = "minhoca";
+                }
+            }
+            Console.WriteLine(animal);
+        }
+    }
+}
