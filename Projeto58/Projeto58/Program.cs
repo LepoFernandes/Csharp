78955c9ac54699cdc1638491321f27e77ff16529
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-05-10 16:44:02 -0300

    estudandooo

---
+++ Projeto58/Projeto58/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace curso
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entradas = Console.ReadLine().Split(' ');
+
+            int X = int.Parse(entradas[0]);
+            int Y = int.Parse(entradas[1]);
+
+            while (X != Y) { 
+            if ( X < Y )
+            {
+                Console.WriteLine("Crescente");                
+                
+            }else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                
+                entradas = Console.ReadLine().Split(' ');
+                X = int.Parse(entradas[0]);
+                Y = int.Parse(entradas[1]);
+            }
+        }
+    }
+}
