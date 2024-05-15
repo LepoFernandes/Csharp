5a548237a8e5ef5bd32cb18793c9c94cf390cd1b
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-05-15 12:00:49 -0300

    Novos exercicios

---
+++ Projeto66/Projeto66/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reposta = 0;
+            int vitoriaInter = 0;
+            int vitoriaGremio = 0;
+            int empate = 0;
+            int totalGrenais = 0;
+            
+
+            while (reposta != 2)
+            {
+
+                string[] gols = Console.ReadLine().Split(' ');
+
+                int golInter = int.Parse(gols[0]);
+                int golGremio = int.Parse(gols[1]);
+
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+
+                reposta = int.Parse(Console.ReadLine());
+
+                if (golInter > golGremio)
+                {
+                    vitoriaInter++;
+                    totalGrenais++;
+                }
+                else if (golInter < golGremio)
+                {
+                    vitoriaGremio++;
+                    totalGrenais++;
+                }
+                else if (golGremio == golInter)
+                {
+                    empate++;
+                    totalGrenais++;
+                }
+
+            }
+
+            
+
+            if(vitoriaGremio > vitoriaInter)
+            {
+                Console.WriteLine(totalGrenais + " grenais");
+                Console.WriteLine("Inter:" + vitoriaInter);
+                Console.WriteLine("Gremio:" + vitoriaGremio);
+                Console.WriteLine("Empates:" + empate);
+                Console.WriteLine("Gremio venceu mais");
+
+            }
+            else if (vitoriaInter > vitoriaGremio)
+            {
+                Console.WriteLine(totalGrenais + " grenais");
+                Console.WriteLine("Inter:" + vitoriaInter);
+                Console.WriteLine("Gremio:" + vitoriaGremio);
+                Console.WriteLine("Empates:" + empate);
+                Console.WriteLine("Inter venceu mais");
+
+            }
+            else if (vitoriaGremio == vitoriaInter)
+            {
+                Console.WriteLine(totalGrenais + " grenais");
+                Console.WriteLine("Inter:" + vitoriaInter);
+                Console.WriteLine("Gremio:" + vitoriaGremio);
+                Console.WriteLine("Empates:" + empate);
+                Console.WriteLine("Nao houve vencedor");
+            }
+
+            
+
+            
+        }
+    }
+}
---
+++ Projeto67/Projeto67/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,resultado;
+
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0)
+            {
+
+                if (x % 2 == 0)
+                {
+                    resultado = (x + (x + 2) + (x + 4) + (x + 6) + (x + 8));
+                }else
+                {
+                    resultado = ((x + 1) + (x + 3) + (x + 5) + (x + 7) + (x + 9));
+                }
+
+                Console.WriteLine(resultado);
+
+                x = int.Parse(Console.ReadLine());
+            }
+
+            
+        }
+    }
+}
