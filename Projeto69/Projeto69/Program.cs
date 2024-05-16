7150aa2b5ef2880d7566246fed35fdb60e8932d8
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-05-16 16:30:42 -0300

    novos exercicios

---
+++ Projeto69/Projeto69/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int resultado = 0;
+
+            if ( N > 2 && N < 1000 )
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    resultado = N * i;
+                    Console.WriteLine(i + " x " + N + " = " + resultado);
+                }
+            }
+        }
+    }
+}
---
+++ Projeto70/Projeto70/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            int somaImpares = 0;
+            int min,max;
+
+            if (X > Y)
+            {
+                 min = Y;
+                 max = X;
+            }else
+            {
+                 min = X;
+                 max = Y;
+            }
+
+            for (int i = min+1; i < max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    somaImpares += i;
+                }
+            }
+
+            Console.WriteLine(somaImpares);
+        }
+    }
+}
