76ae526bed8f26569f1cbad3e283ae2851b2ea12
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-05-23 12:46:51 -0300

    exercicios

---
+++ Projeto81/Projeto81/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double divisao = 0.0;
+
+            for(int i = 0; i < N; i++)
+            {
+                string[] entradas = Console.ReadLine().Split(' ');
+
+                int X = int.Parse(entradas[0], CultureInfo.InvariantCulture);
+                int Y = int.Parse(entradas[1], CultureInfo.InvariantCulture);
+
+                if(Y != 0)
+                {
+                    divisao =(double) X / Y;
+                    Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+                }else
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+            }
+        }
+    }
+}
---
+++ Projeto82/Projeto82/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            int menor = 0;
+            int soma = 0;
+
+            if (X > Y)
+            {
+                menor = Y;
+                Y = X;
+                X = menor;
+            }
+
+            for (int i = X; i <= Y; i++)
+            {
+                 if (i % 13 != 0)
+                {
+                    soma += i;
+                }   
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
