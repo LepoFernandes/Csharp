9ecf185661638861e08130e7f457b47968e71964
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-06-14 12:12:26 -0300

    new

---
+++ Projeto92/Projeto92/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+               
+                string[] entradas = Console.ReadLine().Split(' ');
+
+                int M = int.Parse(entradas[0]);
+                int N = int.Parse(entradas[1]);
+                
+
+                
+                if (M <= 0 || N <= 0)
+                {
+                    break;
+                }
+
+               
+                int maior = M > N ? M : N;
+                int menor = M < N ? M : N;
+
+                int soma = 0;
+
+              
+                for (int i = menor; i <= maior; i++)
+                {
+                    Console.Write(i + " ");
+                    soma += i;
+                }
+
+              
+                Console.WriteLine("Sum=" + soma);
+            }
+
+
+
+        }
+    }
+}
