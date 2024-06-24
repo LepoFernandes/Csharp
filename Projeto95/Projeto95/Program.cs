bfe8530f09360c9b4a01bed424f54efc2951a80e
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-06-24 11:35:13 -0300

    vetores

---
+++ Projeto95/Projeto95/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] B;
+            int N = int.Parse(Console.ReadLine());
+            
+            B = new double[N];
+
+
+            for (int i = 0; i < N; i++)
+            {
+                B[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0;i < N; i++)
+            {
+                Console.WriteLine(B[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
