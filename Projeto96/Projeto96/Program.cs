a2b5ca9e6139e60ca12f80edc3f4611c14ae4079
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-06-25 15:09:09 -0300

    vetores

---
+++ Projeto96/Projeto96/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] vetor;
+            vetor = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                vetor[i] = int.Parse(s[i]);
+               
+            }
+
+            for (int i = 0;i < N; i++)
+            {
+                if (vetor[i] < 0)
+                {
+                    Console.WriteLine(vetor[i]);
+                }
+            }
+
+           
+        }
+    }
+}
