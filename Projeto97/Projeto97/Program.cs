9040f065d997f5b2ccf484d4d094875bd6c4631a
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-06-26 11:42:02 -0300

    exercicios novos

---
+++ Projeto97/Projeto97/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] entradas;
+            double[] entradasINT = new double[N];
+
+            double soma = 0.0;
+            double media = 0.0;
+
+            entradas = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)  
+            {
+                entradasINT[i] = double.Parse(entradas[i], CultureInfo.InvariantCulture);
+                soma = soma + entradasINT[i];
+            }
+
+            media = soma / N;
+
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Projeto98/Projeto98/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DEFININDO AS VARIAVEIS
+            int N = int.Parse(Console.ReadLine());
+            string[] nomes;
+            int[] idades;
+            double[] altura;
+            int menores= 0;
+            double porcentagem = 0.0;
+
+
+            //INSTANCIANDO AS VARIAVEIS
+            nomes = new string[N];
+            idades = new int[N];
+            altura = new double[N];
+
+
+            //ENTRADA DOS DADOS
+            for (int i = 0; i < N; i++) 
+            {
+                string[] gerais = Console.ReadLine().Split(' ');
+                nomes[i] = gerais[0];
+                idades[i] = int.Parse(gerais[1]);
+                altura[i] = double.Parse(gerais[2], CultureInfo.InvariantCulture);
+                
+            }
+
+            //SOMANDO AS ALTURAS E FAZENDO A MEDIA 
+            double soma = 0.0;
+
+            for (int i = 0;i < N; i++)
+            {
+                soma = soma + altura[i];
+            }
+
+            double media = 0.0;
+            media = soma / N;
+
+
+            //FAZENDO A PORCENTAGEM DE MENORES DE 16
+            for (int i = 0;i < N ; i++)
+            {
+                if (idades[i] < 16)
+                {
+                     menores++;
+                }
+            }
+
+            porcentagem = ((double)menores / N) * 100.0;
+
+            Console.WriteLine("Altura media: " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Pessoas com menos de 16 anos : " + porcentagem.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
