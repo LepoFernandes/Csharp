c3429b65012c6fa3702072d85e0ee7d81f576156
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-07-15 16:18:18 -0300

    novos exercicios

---
+++ Projeto100/Projeto100/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] entrada;
+            entrada = new int[N];
+            int count = 0; 
+
+            for (int i = 0; i < N; i++)
+            {
+                entrada[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0;i < entrada.Length; i++)
+            {
+                if (entrada[i] % 2 == 0)
+                {
+                    count++;
+                }
+
+            }
+
+            int[] pares = new int[count];
+            int index = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (entrada[i] % 2 == 0)
+                {
+                    pares[index] = entrada[i];
+                    index++;
+                }
+            }
+
+            Console.WriteLine("Os numeros pares sao:");
+            foreach (int elementos in pares)
+            {
+                Console.WriteLine(elementos);
+            }
+
+            Console.WriteLine("A quantidade de pares sao:");
+            Console.WriteLine(count);
+
+        }
+    }
+}
---
+++ Projeto99/Projeto99/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            double[] entradas;
+            entradas = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                entradas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double maior = entradas[0];
+            int posicao = 0;
+
+            for (int i = 1; i < entradas.Length; i++) 
+            {
+                if (entradas[i] > maior)
+                {
+                    maior = entradas[i];
+                    posicao = i;
+                }
+            }
+
+            Console.WriteLine(maior.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine(posicao);
+
+        }
+    }
+}
