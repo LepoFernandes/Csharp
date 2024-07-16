1bf927dca640de292c162a69b4c158f7e13e150d
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-07-16 11:30:52 -0300

    novos exercicios

---
+++ Projeto101/Projeto101/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantas entradas deseja?");
+
+            int N = int.Parse(Console.ReadLine());
+
+            int[] A = new int[N];
+            int[] B = new int[N];
+            int[] C = new int[N];
+            int index = 0;
+
+            Console.WriteLine("Entre com os numeros:");
+
+            string[] entradas1 = Console.ReadLine().Split(' ');
+
+            string[] entradas2 = Console.ReadLine().Split(' ');
+
+            for(int i = 0;i < entradas1.Length; i++)
+            {
+                A[i] = int.Parse(entradas1[i]);
+            }
+
+            for(int i = 0; i < entradas2.Length; i++)
+            {
+                B[i] = int.Parse(entradas2[i]);
+            }
+
+            for(int i = 0;i < N ; i++)
+            {
+                C[index] = A[i] + B[i];
+                index++;
+            }
+
+            Console.WriteLine("Os resultados sao:");
+
+            foreach (int elementos in C)
+            {
+                Console.WriteLine(elementos);
+            }
+        }
+    }
+}
+
---
+++ Projeto102/Projeto102/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com a quantia de elementos:");
+
+            int N = int.Parse(Console.ReadLine());
+            double soma = 0.0;
+            double media = 0.0;
+
+            Console.WriteLine("Digite as entradas:");
+
+            string[] entradas = Console.ReadLine().Split(' ');
+
+            double[] B = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                B[i] = double.Parse(entradas[i],CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0;i < N; i++)
+            {
+                soma += B[i];
+            }
+
+            media = soma / N;
+
+            Console.WriteLine(media.ToString("F3", CultureInfo.InvariantCulture));
+
+            for (int i = 0;i < N ; i++)
+            {
+                if (B[i] < media)
+                {
+                    Console.WriteLine(B[i].ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
