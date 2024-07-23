82be8021bc0f874cd7d0a3605efbfa732c4a75b3
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-07-23 15:52:11 -0300

    exercicios

---
+++ Projeto104/Projeto104/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // INICIANDO O NUMERO DE ENTRADAS
+            int N = int.Parse(Console.ReadLine());
+
+            // INSTANCIANDO OS VETORES
+            string[] names = new string[N];
+            int[] age = new int[N];
+            
+            //INICIANDO AS VARIAVEIS PRA SABER O MAIOR NUMERO
+            int maior = age[0];
+            int indiceMaior = 0;
+
+            //LOOP PRAS ENTRADAS E PRA COLOCAR OS VALORES NOS VETORES ORIGINAIS
+            for (int i = 0; i < N; i++) 
+            {
+                string[] pessoas = Console.ReadLine().Split(' ');
+
+                names[i] = pessoas[0];
+                age[i] = int.Parse(pessoas[1]);
+            }
+            //LOOP PARA SABER O MAIOR NUMERO E LIGAR COM O NOME USANDO O INDICEMAIOR
+            for (int i = 0;i < age.Length; i++)
+            {
+                if (age[i] > maior)
+                {
+                    maior = age[i];
+                    indiceMaior = i;
+                }
+            }
+
+
+            Console.WriteLine("A pessoa mais velha : " + names[indiceMaior]);
+
+
+
+
+
+            
+
+            
+
+
+        }
+    }
+}
---
+++ Projeto105/Projeto105/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INICIANDO QUANTAS ENTRADAS
+
+            int N = int.Parse(Console.ReadLine());
+
+            //INICIANDO OS VETORES 
+
+            string[] names = new string[N];
+            double[] value1 = new double[N];
+            double[] value2 = new double[N];
+            double[] average = new double[N];
+            
+            //VARIAVEL PRA SABER QUANTOS APROVADOS
+            int NumberApproved = 0;
+            int index = 0;
+
+            //LOOP PARA PEGAR AS ENTRADAS E COLOCAR NOS LUGARES CERTOS
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] entry = Console.ReadLine().Split(' ');
+                names[i] = entry[0];
+                value1[i] = double.Parse(entry[1] , CultureInfo.InvariantCulture);
+                value2[i] = double.Parse(entry[2], CultureInfo.InvariantCulture);
+                
+            }
+            
+            //CALCULANDO A MEDIA
+
+            for (int i = 0;i < N; i++)
+            {
+                average[i] = (value1[i] + value2[i]) / 2;
+            }
+
+            //SABER QUANTOS APROVADOS
+
+            for (int i = 0; i < N; i++)
+            {
+                if (average[i] >= 6.0)
+                {
+                    NumberApproved++;
+                }
+            }
+
+            //COLOCANDO O NOME DOS APROVADOS NO VETOR
+
+            string[] approved = new string[NumberApproved];
+
+            for (int i = 0; i < N; i++)
+            {
+                if (average[i] >= 6.0)
+                {
+                    approved[index] = names[i];
+                    index++;
+                }
+            }
+
+            foreach (string elements in approved)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+    }
+}
