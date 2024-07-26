c3ff432243b4a9849df0c333f080734ded653243
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-07-26 12:08:59 -0300

    novos exercicios

---
+++ Projeto106/Projeto106/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //ENTRANDO COM O TOTAL
+
+            int N = int.Parse(Console.ReadLine());
+
+            string[] entradas = new string[N];
+
+            //ENTRANDO COM OS DADOS NOS VETORES
+
+            double[] alturas = new double[N];
+            string[] sexo = new string[N];
+            
+
+            for (int i = 0;i < N; i++)
+            {
+                entradas = Console.ReadLine().Split(' ');
+                alturas[i] = double.Parse(entradas[0], CultureInfo.InvariantCulture);
+                sexo[i] = entradas[1];
+            }
+
+            //DESCOBRINDO A MAIOR E MENOR ALTURA
+
+            double maior = alturas[0];
+            double menor = alturas[0];
+
+            for (int i = 0;i < alturas.Length; i++)
+            {
+                if (alturas[i] > maior)
+                {
+                    maior = alturas[i];
+                }
+            }
+
+            for (int i = 0;i < alturas.Length ; i++)
+            {
+                if (alturas[i] < menor)
+                {
+                    menor = alturas[i];
+                }
+            }
+
+            //FAZENDO A MEDIA DAS MULHERES
+
+            double soma = 0.0;
+            int countF = 0;
+
+            for ( int i = 0;i < N ; i++)
+            {
+                if (sexo[i] == "F")
+                {
+                    countF++;
+                    soma += alturas[i];
+                }
+            }
+
+            double media = soma / countF;
+
+            //QUANTOS HOMENS
+
+            int countM = 0;
+
+            for ( int i = 0; i < N ; i++)
+            {
+                if (sexo[i] == "M")
+                {
+                    countM++;
+                }
+            }
+
+
+            Console.WriteLine("Menor Altura: " + menor);
+            Console.WriteLine("Maior Altura: " + maior);
+            Console.WriteLine("Media das alturas femininas: " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Numero de homens: " + countM);
+        }
+    }
+}
---
+++ Projeto107/Projeto107/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+ namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+
+            int[,] A;
+
+            A = new int[M , N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] linha0 = Console.ReadLine().Split(' ');
+                
+                for (int j = 0; j < N; j++)
+                {
+                    A[i,j] = int.Parse(linha0[j]);
+                }
+               
+            }
+
+            for (int i = 0;i < M; i++)
+            {
+                for (int j = 0;j < N; j++)
+                {
+                    Console.Write(A[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
