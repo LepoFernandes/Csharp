10130829a7da66047174f68cc4dacef0ba086514
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-07-31 16:24:08 -0300

    new

---
+++ Projeto110/Projeto110/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] A;
+
+            A = new int[N,N];
+            int count = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] entradas = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    A[i,j] = int.Parse(entradas[j]);
+                }
+            }
+
+            for (int i = 0;i < N; i++) 
+            { 
+                for(int j = 0;j < N; j++)
+                {
+                    if (A[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Diagonal Principal:");
+
+            for(int i = 0;i < N ; i++)
+            {
+                Console.Write(A[i,i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total de negativos: " + count);
+
+
+        }
+    }
+}
---
+++ Projeto111/Projeto111/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] A = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] entradas = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    A[i,j] = int.Parse(entradas[j]);
+                }
+            }
+
+            int[] linhas = new int[M];
+
+            for (int i = 0;i < M; i++)
+            {
+                int soma = 0;
+
+                for (int j = 0;j < N; j++)
+                {
+                    soma += A[i,j];
+                }
+
+                linhas[i] = soma;
+            }
+
+            for (int i = 0;i < M; i++)
+            {
+                Console.WriteLine(linhas[i]);
+            }
+        }
+    }
+}
