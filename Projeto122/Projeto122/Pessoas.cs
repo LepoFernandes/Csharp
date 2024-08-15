3e7e158c8e3d580cd1bc8f0f9055dc232cd53adb
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-08-15 20:46:33 -0300

    up

---
+++ Projeto122/Projeto122/Pessoas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto122
+{
+    internal class Pessoas
+    {
+        public string Nomes;
+        public int Idade;
+
+    }
+}
---
+++ Projeto122/Projeto122/Program.cs
@@ -0,0 +1,46 @@
+﻿using Projeto122;
+using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoas X, Y;
+            X = new Pessoas();
+            Y = new Pessoas();
+            
+            Console.WriteLine("Dados da primeira pessoa:");
+
+            Console.WriteLine("Nome: ");
+
+            X.Nomes = Console.ReadLine();
+
+            Console.WriteLine("Idade: ");
+
+            X.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+
+            Console.WriteLine("Nome: ");
+
+            Y.Nomes = Console.ReadLine();
+
+            Console.WriteLine("Idade: ");
+
+            Y.Idade = int.Parse(Console.ReadLine());
+
+
+            if (X.Idade > Y.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha : " + X.Nomes);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha : " +  Y.Nomes);
+            }
+
+        }
+    }
+}
