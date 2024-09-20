1fe566cd14c3a5e513facf611fe6c9905860d6ac
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-09-20 10:48:51 -0300

    update

---
+++ Projeto129/Projeto129/Program.cs
@@ -0,0 +1,44 @@
+﻿using Projeto129;
+using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            aluno Exemplo = new aluno();
+
+            Console.Write("Digite o nome do aluno(a) : ");
+            Exemplo.nomeAluno = Console.ReadLine();
+
+            Console.WriteLine("Digite as 3 notas do aluno(a) : ");
+            Exemplo.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Exemplo.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Exemplo.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Exemplo.notaTotal();
+
+            double notaTotal = Exemplo.notaTotal();
+
+            if (notaTotal > 60.0) 
+            {
+                Console.WriteLine("NOTA FINAL: " + notaTotal.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("APROVADO");
+
+            }
+            else
+            {
+                Exemplo.RestanteNota();
+                double restanteNota = Exemplo.RestanteNota();
+
+                Console.WriteLine("NOTA FINAL: " + notaTotal.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + restanteNota.ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+            
+
+        }
+    }
+}
---
+++ Projeto129/Projeto129/aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Projeto129
+{
+    internal class aluno
+    {
+        public string nomeAluno;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+        
+
+        public double notaTotal()
+        {
+            return nota1 + nota2 + nota3;
+        }
+
+        public double RestanteNota()
+        {
+            return 60.0 - notaTotal();
+        }
+    }
+}
