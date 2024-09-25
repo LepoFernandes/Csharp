ad384ce833cea0a269bd7101bf5e37ee297eab81
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-09-25 14:42:46 -0300

    novo

---
+++ Projeto135/Projeto135/Program.cs
@@ -0,0 +1,48 @@
+﻿using Projeto135;
+using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preco: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            
+
+            Produto p = new Produto(nome, preco, 5);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o numero de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+
+            p.AdicionarProduto(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o numero de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+
+            p.RemoverProduto(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+        }
+    }
+}
