d980e84c6341c11cc0c1688fc32945844034ce40
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-01 16:14:43 -0300

    Update Program.cs

--- Projeto136/Projeto136/Program.cs
+++ Projeto136/Projeto136/Program.cs
@@ -9,39 +9,12 @@ namespace curso
         static void Main(string[] args)
         {
 
-            Produto p = new Produto();
+            Produto p = new Produto("TV" , 500.00 , 10);
 
-            Console.WriteLine("Entre os dados do produto:");
+            p.SetNome("TELEVISAO");
 
-            Console.Write("Nome: ");
-            p.Nome = Console.ReadLine();
-
-            Console.Write("Preço: ");
-            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.Write("Quantidade no estoque: ");
-            p.Quantidade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-
-            p.AdicionarProdutos(qte);
-            Console.WriteLine();
-
-            Console.WriteLine("Dados atualizados: " + p);
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-
-            p.RemoverProdutos(qte);
-            Console.WriteLine();
-
-            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetNome());
         }
     }
 }
