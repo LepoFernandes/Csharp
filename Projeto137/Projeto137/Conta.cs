d009f9e9452dd0f76cec6bb45a3770d989679225
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-02 12:38:24 -0300

    update

---
+++ Projeto137/Projeto137/Conta.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Projeto137
+{
+    internal class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta() { }
+        public Conta (int numeroConta)
+        {
+            NumeroConta = numeroConta;
+        }
+
+        public Conta(int numeroConta, string nomeTitular, double saldo) : this(numeroConta)
+        {
+            NomeTitular = nomeTitular;
+            Saldo = saldo;
+        }
+
+        public double deposito( double valorDeposito)
+        {
+            return Saldo += valorDeposito;
+        }
+        public double saque(double valorSaque)
+        {
+            Saldo = Saldo - valorSaque - 5.0;
+            return Saldo;
+        }
+        public override string ToString()
+        {
+            return
+                "Nome: " +
+                NomeTitular +
+                ", Conta: " +
+                NumeroConta +
+                ", Saldo: " +
+                Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Projeto137/Projeto137/Program.cs
@@ -0,0 +1,103 @@
+﻿using Projeto137;
+using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Bem vindo(a) vamos iniciar seu cadastro.");
+
+            Console.Write("Entre com o numero da sua conta: ");
+
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Conta x = new Conta (numeroConta);
+
+            Console.WriteLine();
+
+            Console.Write("Agora digite o nome do titular da conta: ");
+
+            x.NomeTitular = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Quer fazer um depósito inicial Sr." + x.NomeTitular + " ? (S/N)");
+            
+            string resposta = Console.ReadLine();
+
+            Console.WriteLine();
+
+            if (resposta == "S")
+            {
+                Console.Write("Digite o valor do depósito: ");
+                double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.deposito(valorDeposito);
+            }
+            else if (resposta == "N") 
+            {
+                double saldo = 0.0;
+                Console.WriteLine("O depósito inicial não é obrigatório, mas é vantajoso considerar a possibilidade de depositar um valor no futuro.");
+            } else
+            {
+                Console.WriteLine("Resposta invalida!");
+            }
+
+            Console.WriteLine("Dados da conta: ");
+
+            Console.WriteLine(x.ToString());
+
+
+            Console.WriteLine("Deseja fazer um depósito ou um saque? (1-Depósito / 2- Saque)");
+
+            resposta = Console.ReadLine();
+
+            if (resposta == "1") 
+            {
+                Console.Write("Digite o valor do depósito: ");
+                double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.deposito(valorDeposito);
+
+                Console.WriteLine("Dados da conta: ");
+
+                Console.WriteLine(x.ToString());
+
+
+            }
+            else if (resposta == "2")
+            {
+                Console.WriteLine("Digite o valor do saque: ");
+                double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.saque(valorSaque);
+
+
+                if (valorSaque > x.Saldo) 
+                {
+                    Console.WriteLine("Atenção: seu saldo ficará negativo, o que poderá acarretar em juros.");
+                }
+
+
+                Console.WriteLine("Dados da conta: ");
+
+                Console.WriteLine(x.ToString());
+
+            }
+            else
+            {
+                Console.WriteLine("Comando invalido!");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
