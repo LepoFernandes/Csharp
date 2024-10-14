f3d92b85b36dbb89e2aa2c3d9a67cdd558ef0fbd
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-14 10:59:45 -0300

    update

---
+++ Projeto138/Projeto138/Program.cs
@@ -0,0 +1,17 @@
+﻿using Projeto138;
+using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point p;
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+        }
+    }
+}
---
+++ Projeto138/Projeto138/point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto138
+{
+    struct point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+    }
+}
