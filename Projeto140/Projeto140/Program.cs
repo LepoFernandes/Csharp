2f6d9c6ad7f3110b4960e19f10c0a63c12078632
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-16 11:19:38 -0300

    vetores

---
+++ Projeto140/Projeto140/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+
+            double[] A = new double[N];
+            double average = 0.0;
+
+            for (int i = 0; i < N; i++) 
+            {
+                A[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                average += A[i];
+            }
+
+            average = average / N;
+
+            Console.WriteLine("Average Height = " + average.ToString("F2" , CultureInfo.InvariantCulture));
+
+        }
+    }
+}
