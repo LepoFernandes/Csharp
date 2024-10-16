8b10b2d29c1514034594116972f9db20d6c6ff32
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-16 10:54:07 -0300

    nullable

---
+++ Projeto139/Projeto139/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<double> x = null;
+
+            double? y = null;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            Console.WriteLine(x.Value);
+            Console.WriteLine(y.Value);
+
+
+        }
+    }
+}
