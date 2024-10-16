3e665c854d8565a450a04f0f6f262125082f1f36
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-16 11:55:00 -0300

    update

---
+++ Projeto141/Projeto141/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto141
+{
+    internal class Produto
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ Projeto141/Projeto141/Program.cs
@@ -0,0 +1,35 @@
+﻿using Projeto141;
+using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            Produto[] vet = new Produto[N];
+
+            for (int i = 0; i < N; i++) 
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vet[i] = new Produto { Name = name, Price = price };
+
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < N; i++)
+            {
+                sum += vet[i].Price;
+            }
+
+            double average = sum / N;
+
+            Console.WriteLine("Average price: " + average.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
