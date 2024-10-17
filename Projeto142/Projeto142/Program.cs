6a83613460eaaebfa56f5c30604ba5af02dd083b
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-17 16:57:44 -0300

    update

---
+++ Projeto142/Projeto142/Program.cs
@@ -0,0 +1,73 @@
+﻿using Projeto142;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            hospedes[] quartos = new hospedes[10];
+
+            for (int i = 0; i < quartos.Length; i++)
+            {
+                quartos[i] = null;
+            }
+
+            Console.WriteLine("Quantos hospedes deseja alocar?");
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Agora vamos cadastrar o hospede.");
+
+                Console.WriteLine("Escolha um quarto e complete o cadastro: ");
+
+
+
+                Console.WriteLine("Qual numero do quarto?");
+
+                int numQuarto = int.Parse(Console.ReadLine());
+
+                if (quartos[numQuarto] == null)
+                {
+                    hospedes x = new hospedes();
+
+                    Console.WriteLine("Quarto disponivel vamos seguir.");
+
+                    Console.Write("Digite seu nome: ");
+                    x.Name = Console.ReadLine();
+
+                    Console.Write("Qual seu email: ");
+                    x.Email = Console.ReadLine();
+
+                    quartos[numQuarto] = new hospedes { Email = x.Email, Name = x.Name, NomeQuarto = "quarto " + numQuarto };
+
+                    Console.WriteLine(x.Name + " cadastrado no quarto " + numQuarto + ".");
+                }
+                else
+                {
+                    Console.WriteLine("Quarto ocupado.");
+                }
+
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+
+            for (int i = 0; i < quartos.Length; i++)
+            {
+
+                if (quartos[i] != null)
+                {
+                    Console.WriteLine("Quarto " + i + ": " + quartos[i].Name + ", " + quartos[i].Email + ".");
+                }
+            }
+
+        }
+    }
+}
---
+++ Projeto142/Projeto142/hospedes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto142
+{
+    internal class hospedes
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string NomeQuarto { get; set; }
+
+        public hospedes() { }
+        public hospedes(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        
+    }
+}
