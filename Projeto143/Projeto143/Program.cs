3d6695f21f9098052e8e86aab190ee3066888b42
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-22 13:07:02 -0300

    update

---
+++ Projeto143/Projeto143/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+            list.Add("5");
+
+            foreach (string item in list) 
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
