e75cf6d9a04f220422dacf6b68644fcdff88ac1f
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-24 11:42:34 -0300

    update

---
+++ Projeto144/Projeto144/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Projeto144;
+using System.Diagnostics.SymbolStore;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Quantos empregados quer cadastrar?");
+            int N = int.Parse(Console.ReadLine());
+
+            List<Funcionario> listFuncionarios = new List<Funcionario>();
+
+            for (int i = 0; i < N; i++) 
+            {
+                
+                Console.Write("Qual id do funcionario: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Qual nome do funcionario: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Qual salario do funcionario: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Funcionario X = new Funcionario(id, name, salary);
+
+                listFuncionarios.Add(X); 
+
+                Console.WriteLine("Adicionado com sucesso.");
+                
+                
+
+            }
+
+            Console.WriteLine("Escolha o funcionario que voce quer aumentar o salario: ");
+
+            int codigo = int.Parse(Console.ReadLine());
+
+            Funcionario funcionario = listFuncionarios.Find(x => x.Id == codigo);
+
+            if (listFuncionarios != null)
+            {
+                Console.WriteLine("Qual o percentual: ");
+                double percentual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                funcionario.AumentarSalario(percentual);
+            } else
+            {
+                Console.WriteLine("Funcionario nao encontrado.");
+            }
+
+            foreach (Funcionario X in listFuncionarios)
+            {
+                Console.WriteLine(X.ToString());
+            }
+        }
+    }
+}
---
+++ Projeto144/Projeto144/funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Projeto144
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Funcionario(int id, string name, double salary) 
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void AumentarSalario(double percentual)
+        {
+            
+            Salary += Salary * percentual / 100;
+        }
+
+        public override string ToString() 
+        {
+            return 
+                "Funcionario: " + Name
+                + " ID: " + Id
+                + " Salario: " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
