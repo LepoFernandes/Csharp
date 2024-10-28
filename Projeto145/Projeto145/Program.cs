970d4c4156f466a5deb0865102259c7d5a2958a2
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-28 12:34:59 -0300

    update

---
+++ Projeto145/Projeto145/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[N,N];
+
+            int soma = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] dados = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < N; j++)
+                {                    
+
+                    mat[i, j] = int.Parse(dados[j]);
+                }
+            }
+
+            for (int i = 0; i < N; i++) 
+            {
+                for(int j = 0;j < N; j++)
+                {                   
+                    if (mat[i, j] < 0)
+                    {
+                        soma++;
+                    }
+                }
+            }
+            Console.WriteLine("Main Diagonal: ");
+
+            for (int i = 0; i < N; i++) 
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Negative numbers: ");
+            Console.Write(soma);
+            
+        }
+    }
+}
