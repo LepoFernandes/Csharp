598be8a33018ff094fee288ff93d5177217059ba
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-10-29 14:42:56 -0300

    update

---
+++ Projeto146/Projeto146/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o numero de linhas e colunas: ");
+
+            int M = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[M, N];
+
+            for (int i = 0; i < M; i++) 
+            {
+                string[] entradas = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) 
+                {
+                    matriz[i,j] = int.Parse(entradas[j]);
+                }
+            }
+
+            Console.WriteLine("Qual numero quer desvendar? ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < M; i++) 
+            {
+                for (int j = 0;j < N; j++)
+                {
+                    if (matriz[i,j] == number)
+                    {
+                        Console.WriteLine("Position: " + "(" + i + "," + j + ")");
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + matriz[i - 1, j]);
+                        } if (i < M - 1)
+                        {
+                            Console.WriteLine("Down: " + matriz[i + 1, j]);
+                        } if (j > 0) 
+                        {
+                            Console.WriteLine("Left: " + matriz[i, j - 1]);
+                        } if (j < N - 1)
+                        {
+                            Console.WriteLine("Right: " + matriz[i, j + 1]);
+                        }                                                                                                                     
+                    }
+                }
+            }
+        }
+    }
+}
