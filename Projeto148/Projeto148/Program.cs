14382304c199872dadd1f29567c79be968fcd022
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-11-12 15:55:27 -0300

    update

--- Projeto148/Projeto148/Program.cs
+++ Projeto148/Projeto148/Program.cs
@@ -9,17 +9,17 @@ namespace curso
             DateTime d1 = new DateTime(2024, 11, 12);
             DateTime d2 = new DateTime(2024,11, 12, 14 ,52 , 25);
 
-            Console.WriteLine(d1);
-            Console.WriteLine(d2);
+            Console.WriteLine(d1); //Data com os parametros que passei
+            Console.WriteLine(d2); //Data tbm com os parametros
 
             DateTime d3 = DateTime.Now;
             DateTime d4 = DateTime.Today;
             DateTime d5 = DateTime.UtcNow;
             
 
-            Console.WriteLine(d3);
-            Console.WriteLine(d4);
-            Console.WriteLine(d5);
+            Console.WriteLine(d3); //Data de agora com dia e horario
+            Console.WriteLine(d4); //Data de hoje mas no horario zerado
+            Console.WriteLine(d5); //Data de hoje com horario gmt 
         }
     }
 }
---
+++ Projeto149/Projeto149/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime d1 = DateTime.Parse("1999-09-08");
+            DateTime d2 = DateTime.Parse("1999-09-08 17:30:45");
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+
+            DateTime d3 = DateTime.Parse("08/09/1999");
+            DateTime d4 = DateTime.Parse("08/09/1999 17:30:45");
+
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            DateTime d5 = DateTime.ParseExact("1999-09-08", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime d6 = DateTime.ParseExact("08/09/1999 17:30:24" , "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture) ;
+
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+        }
+    }
+}
