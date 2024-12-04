19c8574b38af7e583eca2a3fed2ff7eb0d94b550
Lucas Fernandes <202311090795@alunos.estacio.br>
2024-12-04 16:09:48 -0300

    enums

---
+++ Projeto157/Projeto157/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Projeto157.Entities;
+using Projeto157.Entities.Enums;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+
+        }
+    }
+}
