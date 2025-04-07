de32d2d10cd58157a55eb298278b3ca7682a4492
Lucas Fernandes <202311090795@alunos.estacio.br>
2025-04-07 14:32:46 -0300

    FIle

---
+++ Projeto177/Projeto177/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+
+            
+            StreamReader sr = null;
+
+            try
+            {
+                
+                sr = File.OpenText(path);
+
+                string line = sr.ReadToEnd();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+                
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+               
+            }
+        }
+    }
+}
