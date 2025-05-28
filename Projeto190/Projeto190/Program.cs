75e100bd90fc13f763e67952d156bc335af541a4
Lucas Fernandes <202311090795@alunos.estacio.br>
2025-05-28 11:11:31 -0300

    update

---
+++ Projeto190/Projeto190/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Projeto190.Entities;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Client A = new Client{Name = "Maria", Email = "maria@gmail.com"};
+
+            Client B = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            
+            Console.WriteLine(A.Equals(B));
+            Console.WriteLine(A == B);
+            Console.WriteLine(A.GetHashCode());
+            Console.WriteLine(B.GetHashCode());
+        }
+    }
+}
