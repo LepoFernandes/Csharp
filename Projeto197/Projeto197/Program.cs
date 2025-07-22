47dd86ed00bd8c2b79a4294128a5d75affa4105b
Lucas Fernandes <202311090795@alunos.estacio.br>
2025-07-22 17:37:50 -0300

    update

---
+++ Projeto197/Projeto197/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+using System.Collections.Generic;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Dictionary<string, int> dados1 = new Dictionary<string, int>();
+
+            Console.WriteLine("Enter file full path: ");
+
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        
+
+                        if (dados1.ContainsKey(name))
+                        {
+                            dados1[name] += votes;
+                        }
+                        else
+                        {
+                            dados1[name] = votes;
+                        }
+                        
+                    }
+                }
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine(e.Message);   
+            }
+
+            Console.WriteLine("Final Count: ");
+
+            foreach(KeyValuePair<string, int> dados in dados1)
+            {
+                Console.WriteLine(dados.Key + ": " + dados.Value);
+            }
+        }
+    }
+}
