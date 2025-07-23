27ecf21645ba66ae8076e91e32f6510a030f3dca
Lucas Fernandes <202311090795@alunos.estacio.br>
2025-07-23 11:51:59 -0300

    update

---
+++ Projeto198/Projeto198/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto198.Extensions
+{
+    static class StringExtensions
+    {
+        public static string Cut(this string ThisObj, int count)
+        {
+            if (ThisObj.Length <= count)
+            {
+                return ThisObj;
+            }
+            else
+            {
+              return ThisObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
---
+++ Projeto198/Projeto198/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Projeto198.Extensions;
+
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            DateTime dt = new DateTime(2025, 07, 23, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students.";
+            Console.WriteLine(s1.Cut(15));
+        }
+    }
+}
