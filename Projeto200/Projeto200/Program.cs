30dfc6e5b8c766634c6110353b0e56cca1eb47be
Lucas Fernandes <202311090795@alunos.estacio.br>
2025-07-25 12:00:10 -0300

    new projects

---
+++ Projeto200/Projeto200/Program.cs
@@ -0,0 +1,45 @@
+﻿using Projeto200.Services;
+using System;
+
+namespace Curso
+{
+    /*  JEITO NORMAL SEM MULTICAST
+    class Program
+    {
+
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args) 
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+
+            Console.WriteLine(result);
+
+        }
+    }
+
+    */
+
+    class Program
+    {
+
+        delegate void BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args) 
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+
+            op += CalculationService.ShowMax;
+
+            op(a, b);
+        }
+    }
+}
