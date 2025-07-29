8d67eb4855cb0103a488563e0b8a4d99d8229df2
Lucas Fernandes <202311090795@alunos.estacio.br>
2025-07-29 20:40:53 -0300

    update

---
+++ Projeto202/Projeto202/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Projeto202.Entities;
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.90));
+
+
+            Action<Product> act = p => { p.Price += p.Price * 0.1; };
+
+            list.ForEach(act);
+
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+        }
+
+        
+    }
+}
---
+++ Projeto203/Projeto203/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Projeto203.Entities;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Func<Product, string> func = p => p.Name.ToUpper();
+
+            List<string> result = list.Select(func).ToList();
+
+
+            foreach(Product product in list)
+            {
+                Console.WriteLine(product.ToString());
+            }
+
+            Console.WriteLine();
+
+            foreach (String p in result)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+            
+    }
+}
